4663343da6a9388fa8062c429ec9a0230362fcee
PantaLeao10 <pantaleo.giovanni@darzo.net>
2024-05-24 11:50:21 +0100

    Prova cambio colore

---
+++ cambioColore.js
@@ -0,0 +1,44 @@
+function setupImageChange1() {
+    var img = document.getElementsById("slide-1");
+    img.src = "..\Vestiti\Bambini\Body\organic-cotton-baby-bodysuit-organic-natural-front-65c1e8ee8938a.jpg" ;
+}
+
+function setupImageChange2() {
+    var img = document.getElementById('bianco1');
+    img.src = 'Vestiti\Bambini\Body\organic-cotton-baby-bodysuit-white-right-front-65c1e8ee895db.jpg';
+}
+
+function setupImageChange3() {
+    var img = document.getElementById('bianco2');
+    img.src = 'Vestiti\Bambini\Body\organic-cotton-baby-bodysuit-white-front-65c1e8ee89795.jpg';
+}
+function setupImageChange4() {
+    var img = document.getElementById('image');
+    img.src = 'img1';
+}
+function setupImageChange5() {
+    var img = document.getElementById('image');
+    img.src = 'img1';
+}
+function setupImageChange6() {
+    var img = document.getElementById('image');
+    img.src = 'img1';
+}
+function setupImageChange7() {
+    var img = document.getElementById('image');
+    img.src = 'img1';
+}
+function setupImageChange8() {
+    var img = document.getElementById('image');
+    img.src = 'img1';
+}
+
+function setupImageChange9() {
+    var img = document.getElementById('image');
+    img.src = 'img1';
+}
+
+function setupImageChange10() {
+    var img = document.getElementById('image');
+    img.src = 'img1';
+}
