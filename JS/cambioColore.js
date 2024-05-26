490d13511a8c2c34f5c92c02779648ee717a653b
Imraj Singh <singh.imraj@darzo.net>
2024-05-26 02:51:15 +0200

    aggiunto cambio colore per body, logn non funziona per altre pagine aaaaa

---
+++ JS/cambioColore.js
@@ -0,0 +1,67 @@
+function body_to_white() {
+    var img1 = document.getElementById('slide-1');
+    var img2 = document.getElementById('slide-2');
+    var img3 = document.getElementById('slide-3');
+
+    var but = document.getElementById('add_cart');
+    var heading = document.getElementById('heading');
+
+    heading.innerHTML = "Body Bianco NOBLM";
+    but.value = "body-w";
+
+    img1.src = "../Vestiti/Bambini/Body/organic-cotton-baby-bodysuit-white-front-65c1e8ee896bc.jpg";
+    img2.src = "../Vestiti/Bambini/Body/organic-cotton-baby-bodysuit-white-front-65c1e8ee89795.jpg";
+    img3.src = "../Vestiti/Bambini/Body/organic-cotton-baby-bodysuit-white-right-front-65c1e8ee895db.jpg";
+}
+
+function body_to_baige() {
+    var img1 = document.getElementById('slide-1');
+    var img2 = document.getElementById('slide-2');
+    var img3 = document.getElementById('slide-3');
+
+    var but = document.getElementById('add_cart');
+    var heading = document.getElementById('heading');
+
+    heading.innerHTML = "Body Baige NOBLM";
+    but.value = "body-b";
+
+    img1.src = "../Vestiti/Bambini/Body/organic-cotton-baby-bodysuit-organic-natural-front-65c1e8ee8938a.jpg";
+    img2.src = "../Vestiti/Bambini/Body/organic-cotton-baby-bodysuit-organic-natural-right-front-65c1e8ee88a12.jpg";
+    img3.src = "../Vestiti/Bambini/Body/organic-cotton-baby-bodysuit-organic-natural-front-65c1e8ee89474.jpg";
+}
+
+/*function setupImageChange3() {
+    var img = document.getElementById('bianco2');
+    img.src = 'Vestiti/Bambini/Body/organic-cotton-baby-bodysuit-white-front-65c1e8ee89795.jpg';
+}
+function setupImageChange4() {
+    var img = document.getElementById('image');
+    img.src = 'img1';
+}
+
+function setupImageChange5() {
+    var img = document.getElementById('image');
+    img.src = 'img1';
+}
+function setupImageChange6() {
+    var img = document.getElementById('image');
+    img.src = 'img1';
+}
+function setupImageChange7() {
+    var img = document.getElementById('image');
+    img.src = 'img1';
+}
+function setupImageChange8() {
+    var img = document.getElementById('image');
+    img.src = 'img1';
+}
+
+function setupImageChange9() {
+    var img = document.getElementById('image');
+    img.src = 'img1';
+}
+
+function setupImageChange10() {
+    var img = document.getElementById('image');
+    img.src = 'img1';
+}**/
---
+++ JS/script.js
@@ -0,0 +1,81 @@
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBR_qoESMD-KdXClF2vURe5crAY1CAo15g",
+    authDomain: "noblm-7dd76.firebaseapp.com",
+    projectId: "noblm-7dd76",
+    storageBucket: "noblm-7dd76.appspot.com",
+    messagingSenderId: "651741143531",
+    appId: "1:651741143531:web:2f3b274db7008b10ccb9ae"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const profile_but = document.getElementById('profile');
+const cart_but = document.getElementById('cart');
+const login_but = document.getElementById('login');
+const login_shop = document.getElementById('login_to_shop');
+const profileDropdown = document.getElementById('profileDropdown');
+const logoutButton = document.getElementById('logout');
+
+// Add event listener to the logout button
+logoutButton.addEventListener('click', () => {
+    signOut(auth).then(() => {
+        // Sign-out successful.
+        // After sign-out, you may want to redirect the user to a different page or perform any other action.
+        console.log("User signed out successfully");
+    }).catch((error) => {
+        // An error happened.
+        console.error("Error signing out:", error);
+    });
+});
+
+profile_but.addEventListener('click', () => {
+    profileDropdown.style.display = profileDropdown.style.display === 'block' ? 'none' : 'block';
+});
+
+logoutButton.addEventListener('click', () => {
+    profileDropdown.style.display = 'none';
+});
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is signed in
+        cart_but.style.display = 'block';
+        profile_but.style.display = 'block';
+        login_but.style.display = 'none';
+        login_shop.value = "Shop";
+        login_shop.textContent = "Shop"; 
+
+    } else {
+        // No user is signed in
+        cart_but.style.display = 'none';
+        profile_but.style.display = 'none';
+        login_but.style.display = 'block';;
+        login_shop.value = "Login";
+        login_shop.textContent = "Login"; 
+    }
+});
+
+
