03ed867b88c93382114ea43e1ee0613a43ab0343
Imraj Singh <singh.imraj@darzo.net>
2024-05-27 00:21:46 +0200

    aggiunta login e robe varie

--- JS/cambioColore.js
+++ JS/cambioColore.js
@@ -62,6 +62,83 @@ function felpa_to_black() {
     img3.src = "../Vestiti/Bambini/Felpa-cappuccio/youth-heavy-blend-hoodie-black-front-65af796524ccc.jpg";
 }
 
+function felpa_to_black() {
+    var img1 = document.getElementById('slide-1');
+    var img2 = document.getElementById('slide-2');
+    var img3 = document.getElementById('slide-3');
+    var but = document.getElementById('add_cart');
+    var heading = document.getElementById('heading');
+    var price = document.getElementById('price_felpa');
+
+    heading.innerHTML = "Felpa Nera NOBLM";
+    but.value = "felpa_black";
+    price.innerHTML = "€29.99";
+
+    img1.src = "../Vestiti/Bambini/Felpa-cappuccio/youth-heavy-blend-hoodie-black-back-65af7965252a9.jpg";
+    img2.src = "../Vestiti/Bambini/Felpa-cappuccio/youth-heavy-blend-hoodie-black-front-2-65af796524fef.jpg";
+    img3.src = "../Vestiti/Bambini/Felpa-cappuccio/youth-heavy-blend-hoodie-black-front-65af796524ccc.jpg";
+}
+
+function polo_to_grigio() {
+    var img1 = document.getElementById('slide-1');
+    var img2 = document.getElementById('slide-2');
+    var img3 = document.getElementById('slide-3');
+    var but = document.getElementById('add_cart');
+    var heading = document.getElementById('heading');
+
+    heading.innerHTML = "Polo Grigio NOBLM";
+    but.value = "polo_grigio";
+
+    img1.src = "../Vestiti/Unisex/Polo/unisex-pique-polo-shirt-sport-grey-front-65c1ebd1222f3.jpg";
+    img2.src = "../Vestiti/Unisex/Polo/unisex-pique-polo-shirt-sport-grey-front-65c1ebd12201f.jpg";
+    img3.src = "../Vestiti/Unisex/Polo/unisex-pique-polo-shirt-sport-grey-product-details-65c1ebd0d37c9.jpg";
+}
+
+function polo_to_white() {
+    var img1 = document.getElementById('slide-1');
+    var img2 = document.getElementById('slide-2');
+    var img3 = document.getElementById('slide-3');
+    var but = document.getElementById('add_cart');
+    var heading = document.getElementById('heading');
+
+    heading.innerHTML = "Polo Bianca NOBLM";
+    but.value = "polo_white";
+
+    img1.src = "../Vestiti/Unisex/Polo/unisex-pique-polo-shirt-white-front-65c1ebd122b63.jpg";
+    img2.src = "../Vestiti/Unisex/Polo/unisex-pique-polo-shirt-white-front-65c1ebd12278e.jpg";
+    img3.src = "../Vestiti/Unisex/Polo/unisex-pique-polo-shirt-white-product-details-65c1ebd1226b9.jpg";
+}
+
+function berretto_to_pink() {
+    var img1 = document.getElementById('slide-1');
+    var img2 = document.getElementById('slide-2');
+    var img3 = document.getElementById('slide-3');
+    var but = document.getElementById('add_cart');
+    var heading = document.getElementById('heading');
+
+    heading.innerHTML = "Berretto Rosa NOBLM";
+    but.value = "berretto-pink";
+
+    img1.src = "../Vestiti/Unisex/Berretto/cuffed-beanie-baby-pink-front-65af7b6caf47e.jpg";
+    img2.src = "../Vestiti/Unisex/Berretto/cuffed-beanie-baby-pink-front-65af7b6caf447.jpg";
+    img3.src = "../Vestiti/Unisex/Berretto/cuffed-beanie-baby-pink-front-65af7b6caf41b.jpg";
+}
+
+function berretto_to_grey() {
+    var img1 = document.getElementById('slide-1');
+    var img2 = document.getElementById('slide-2');
+    var img3 = document.getElementById('slide-3');
+    var but = document.getElementById('add_cart');
+    var heading = document.getElementById('heading');
+
+    heading.innerHTML = "Berretto Grigio NOBLM";
+    but.value = "berretto-grey";
+
+    img1.src = "../Vestiti/Unisex/Berretto/cuffed-beanie-heather-grey-front-65af7b6caf2a9.jpg";
+    img2.src = "../Vestiti/Unisex/Berretto/cuffed-beanie-heather-grey-front-65af7b6caf22b.jpg";
+    img3.src = "../Vestiti/Unisex/Berretto/cuffed-beanie-heather-grey-front-65af7b6caf1e1.jpg";
+}
+
 /*function setupImageChange3() {
     var img = document.getElementById('bianco2');
     img.src = 'Vestiti/Bambini/Body/organic-cotton-baby-bodysuit-white-front-65c1e8ee89795.jpg';
